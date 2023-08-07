a574d7eeb49bf3d6f7e00568d7192eeeb6327e66
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-07 10:35:16 -0300

    07/08

--- pepe/App.js
+++ pepe/App.js
@@ -1,20 +1,38 @@
+import React from 'react';
+import { useState } from "react"
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, View, ImageBackground, Button, Alert, TouchableOpacity} from 'react-native';
+import imagenLocal_react from'./assets/pipo.png';
 
-const imagenLocal_react= require('./assets/pipo.png');
+
 export default function App() {
+  const textInicial = "A nacho le gusta mucho la pizza..."
+  const textCompleto = "A nacho le gusta mucho la pizza con mayonesa"
+  const [nombre, handleNombreChange] = React.useState('');
+  const [count, setCount] = useState(0);  
+  const handlePress = () => setCount(count + 1);
   return (
-    <View style={styles.container}>
-      <Text>Ignacio</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
+      <Text style={styles.titleText}>Ignacio</Text>
+      <Text style={styles.titleText}> {textInicial} {'\n'}{'\n'}</Text>
+      <Text style={styles.bodyText} numberOfLines={2}> {textCompleto} </Text>
       <ImageBackground source={imagenLocal_react} 
       resizeMode="cover"
-      style={styles.backgroundImage}>
+      style={styles.backgroundImage}></ImageBackground>
+      
+      <TouchableOpacity style={styles.button} onPress={handlePress}>        
+      <Text style={styles.bodyText}>Presioname</Text>      
+      </TouchableOpacity>
+      <Text style={styles.bodyText} numberOfLines={2}> {count} </Text>
 
-
-
-      </ImageBackground>
-    </View>
+      <TextInput        
+      style={styles.input}        
+      placeholder="ingrese..."        
+      onChangeText={handleNombreChange}/>
+      <Text style={styles.bodyText} numberOfLines={2}> {nombre} </Text>
+      <Button style={styles.button} title="Presioname"    onPress={() => Alert.alert('Me apretaste!')} />
+    </SafeAreaView>
   );
 }
 
@@ -24,9 +42,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 5
+    ,
+    
+    
   },
   backgroundImage: {
     flex: 1,
     justifyContent: 'center',
   },
+  bodyText: {fontFamily: 'arial', flex:2,},  
+  titleText: {fontFamily: 'arial', color: 'red', flex: 3,  
+  fontSize: 20, fontWeight: 'bold',},
+  input: {height: 100, borderWidth: 2, padding: 1, flex:1,},
+  button: { flex:1,},
 });
