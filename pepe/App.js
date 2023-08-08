2614192960857260c89fb2fc52d5eece6853f07b
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-08 09:07:07 -0300

    08/08

--- pepe/App.js
+++ pepe/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from "react"
 import { StatusBar } from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, Text, TextInput, View, ImageBackground, Button, Alert, TouchableOpacity} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, View, ImageBackground, Button, Alert, TouchableOpacity, Pressable, Keyboard} from 'react-native';
 import imagenLocal_react from'./assets/pipo.png';
 
 
@@ -9,8 +9,30 @@ export default function App() {
   const textInicial = "A nacho le gusta mucho la pizza..."
   const textCompleto = "A nacho le gusta mucho la pizza con mayonesa"
   const [nombre, handleNombreChange] = React.useState('');
-  const [count, setCount] = useState(0);  
-  const handlePress = () => setCount(count + 1);
+  const [telefono, handleTelefonoChange] = React.useState('');
+  const [email, handleMailChange] = React.useState('');
+  const [clave, handleClaveChange] = React.useState('');
+  const [count, setCount] = useState(0); 
+  const [texto, setTexto] = useState('no estoy siendo apretado');
+  const handlePress = () => {
+    setCount(count + 1);
+    {/*pusimos que te muestra count +1 porque sino va 1 numero mas atrasado*/ }
+    console.log( "el contador esta en " + (count+1));
+  };
+  const alertar = ()=> { 
+    Alert.alert('me apretaste');
+    console.log("apretaste el button");
+};
+  const presionado = () => {
+    setTexto('estas apretando el pressable');
+    console.log("apretaste el touchable");
+};
+  const noPresionado = () => {
+    setTexto('no estoy siendo apretado');
+    console.log('el touchable dejo de ser apretado')
+};
+  
+  
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
@@ -21,17 +43,42 @@ export default function App() {
       resizeMode="cover"
       style={styles.backgroundImage}></ImageBackground>
       
-      <TouchableOpacity style={styles.button} onPress={handlePress}>        
+      <TouchableOpacity style={styles.button} onPress={handlePress} >        
       <Text style={styles.bodyText}>Presioname</Text>      
       </TouchableOpacity>
       <Text style={styles.bodyText} numberOfLines={2}> {count} </Text>
 
+      <Pressable style={styles.button} onPress={presionado} onPressOut={noPresionado}>  
+      <Text style={styles.bodyText}> {texto} </Text> 
+      </Pressable>
+      {/* esto de abajo son los in*/}
       <TextInput        
       style={styles.input}        
-      placeholder="ingrese..."        
+      placeholder="ingrese nombre"  
+      returnKeyType="next"
       onChangeText={handleNombreChange}/>
-      <Text style={styles.bodyText} numberOfLines={2}> {nombre} </Text>
-      <Button style={styles.button} title="Presioname"    onPress={() => Alert.alert('Me apretaste!')} />
+      <Text style={styles.bodyText} numberOfLines={2}> nombre:{nombre} </Text>
+      <TextInput        
+      style={styles.input}        
+      placeholder="ingrese telefono"  
+      keyboardType="phone-pad"
+      returnKeyType="next"
+      onChangeText={handleTelefonoChange}/>
+      <Text style={styles.bodyText} numberOfLines={2}> telefono: {telefono} </Text>
+      <TextInput        
+      style={styles.input}        
+      placeholder="ingrese email"  
+      keyboardType="email-address"
+      returnKeyType="next"
+      onChangeText={handleMailChange}/>
+      <Text style={styles.bodyText} numberOfLines={2}> email: {email} </Text>
+      <TextInput        
+      style={styles.input}        
+      placeholder="ingrese clave"  
+      returnKeyType="next"
+      onChangeText={handleClaveChange}/>
+      <Text style={styles.bodyText} numberOfLines={2}> clave: {clave} </Text>
+      <Button style={styles.button} title="Soy un botón"    onPress={() => alertar} />
     </SafeAreaView>
   );
 }
@@ -54,6 +101,6 @@ const styles = StyleSheet.create({
   bodyText: {fontFamily: 'arial', flex:2,},  
   titleText: {fontFamily: 'arial', color: 'red', flex: 3,  
   fontSize: 20, fontWeight: 'bold',},
-  input: {height: 100, borderWidth: 2, padding: 1, flex:1,},
+  input: {height: 100, borderWidth: 2, padding: 1, flex:1, },
   button: { flex:1,},
 });
